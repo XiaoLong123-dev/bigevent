9a5ba77faad733417bb66109e39bd72782614b12
lxl <2042984402@qq.com>
2022-05-23 19:04:52 +0800

    完成了登录和注册功能的开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -2,5 +2,8 @@
 // 会先调用ajaxPrefilter这个函数
 // 在这个函数中可以拿到我们给ajax提供的配置方案
 $.ajaxPrefilter(function (options) {
-    // console.log(options.url);
+    // 在发送真正的ajax请求之前 统一拼接请求的根路径
+    options.url="http://www.liulongbin.top:3007"+options.url;
+    console.log(options.url);
+
 })
--- assets/js/login.js
+++ assets/js/login.js
@@ -13,7 +13,6 @@ $(function () {
     // 从layui获取form对象
     var form = layui.form;
     var layer = layui.layer;
-    // layer.msg('注册成功');
     // 通过form.verify()函数自定义校验规则
     form.verify({
         // 自定义pass校验规则 [\S]不能包含空格
@@ -41,7 +40,7 @@ $(function () {
             username: $('#form_reg [name=username]').val(),
             password: $('#form_reg [name=password]').val()
         };
-        $.post('http://www.liulongbin.top:3007/api/reguser', data, function (res) {
+        $.post('/api/reguser', data, function (res) {
             if (res.status !== 0) {
                 return layer.msg(res.message);
             }
@@ -54,13 +53,12 @@ $(function () {
     // 监听登陆表单的提交事件
     $('#form_login').submit(function (e) {
         e.preventDefault();
-        // layer.msg('登陆成功');
         $.ajax({
-            url: 'http://www.liulongbin.top:3007/api/login',
+            url: '/api/login',
             method: 'post',
             // 快速获取表单内容
             data: $(this).serialize(),
-            succes: function (res) {
+            success: function (res) {
                 if (res.status !== 0) {
                     return layer.msg(res.message);
                 }
@@ -69,7 +67,7 @@ $(function () {
                 // 将登陆成功的token字符串，保存到localStorage中
                 localStorage.setItem('token', res.token);
                 // 跳转到index主页
-                location.href = '/index.html';
+                location.href = 'index.html';
             }
         })
     })
